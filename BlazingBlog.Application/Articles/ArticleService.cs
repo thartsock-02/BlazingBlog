2da3501a1ce205c3f443a15ea1571d89e5ab015a
Tyler Hartsock <tyler@solutionsmng.com>
2024-02-17 13:29:18 -0600

    Finish first implementation fo clean arch

---
+++ BlazingBlog.Application/Articles/ArticleService.cs
@@ -0,0 +1,21 @@
+﻿using BlazingBlog.Domain.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBlog.Application.Articles
+{
+    public class ArticleService : IArticleService
+    {
+        public List<Article> GetAllArticles()
+        {
+            return new List<Article>
+            {
+                new Article { Id = 1, Title = "My First Article", Content = " This is my first article."},
+                new Article { Id = 2, Title = "My Second Article", Content = " This is my second article."}
+            };
+        }
+    }
+}
--- BlazingBlog.WebUI.Server/Program.cs
+++ BlazingBlog.WebUI.Server/Program.cs
@@ -1,9 +1,11 @@
 using BlazingBlog.WebUI.Server.Components;
+using BlazingBlog.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
